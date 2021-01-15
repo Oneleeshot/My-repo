eb6f14ccaad775edd734c610f2615246b2b045e7
Oneleeshot <instal9595@gmail.com>
2021-01-15 15:40:31 +0300

    My-repo

--- djangolesson1/book/views.py
+++ djangolesson1/book/views.py
@@ -3,10 +3,6 @@ from .models import Book
 from django.views.generic import DetailView, CreateView, ListView
 
 
-# def books_list(request):
-#     books = Book.objects.all()
-#     return render(request, 'book/index.html', context={'books': books})
-
 class BookListView(ListView):
     model = Book
 
@@ -28,19 +24,3 @@ class BookDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         return context
 
-# def book_create(request):
-#     error = ''
-#     if request.method == 'POST':
-#         form = BookForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('books_list_url')
-#         else:
-#             error = '123'
-#     form = BookForm()
-#     return render(request, 'book/book_create_form.html',
-#                   context={'form': form, 'error': error})
-
-# def book_detail(request, id):
-#     book = Book.objects.get(id=id)
-#     return render(request, 'book/book_detail.html', context={'book': book})
